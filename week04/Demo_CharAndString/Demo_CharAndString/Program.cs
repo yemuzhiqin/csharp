96a9f3044bbc0ad167cf89c42990be2f74608a20
yemuzhiqin <1957960965.qq.com>
2019-03-23 16:37:56 +0800

    未完成的第四周作业

---
+++ week04/Demo_CharAndString/Demo_CharAndString/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo_CharAndString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ///char为值类型，使用单引号（''）  string为引用类型，使用双引号（""）
+            
+            Test t1 = new Test();//引用类型
+            Test t2 = t1;
+
+            Console.WriteLine("t1.x={0},t1.y={1}", t1.x, t1.y);
+            Console.WriteLine("t2.x={0},t2.y={1}", t2.x, t2.y);
+
+            t2.x = 11;
+            t2.y = 22;
+            Console.WriteLine("t1.x={0},t1.y={1}", t1.x, t1.y);
+            Console.WriteLine("t2.x={0},t2.y={1}", t2.x, t2.y);
+
+            string name = "夜幕之琴";
+            char sex = '女';
+            double score = 86.5;
+            string s = name;
+            Console.WriteLine("name={0},sex={1},score={2}",name,sex,score);
+            s = "yemuzhiqin";//这一操作并不会改变name变量的值
+            Console.WriteLine("name={0},sex={1},score={2}", name, sex, score);//此时输出的name值仍然为夜幕之琴
+
+            Console.ReadKey();
+
+        }
+    }
+    class Test
+    {
+        public int x = 10;
+        public int y = 20;
+    }
+}
---
+++ week04/Demo_StringCompare/Demo_StringCompare/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo_StringCompare
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str1 = "abc";
+            string str2 = "Abc";
+            Console.WriteLine("str1==str2的结果为：{0}",str1==str2);
+            Console.WriteLine("str1.Equals(str2)的结果为:{0}", str1.Equals(str2));
+            Console.WriteLine("str1.CompareTo(str2)的结果为：{0}", str1.CompareTo(str2));//若str1==str2,则返回0；若str1<str2，则返回-1；若str1>str2，则返回1
+            Console.WriteLine("string.Compare(str1,str2)的结果为：{0}",string.Compare(str1,str2));//默认比较是区分大小写的；比较同上
+            Console.WriteLine("string.Compare(str1,str2)的结果为：{0}", string.Compare(str1, str2,true));//比较是不区分字符的大小写的
+            Console.WriteLine("str1.ToLower()==str2.ToLower()的结果为：{0}", str1.ToLower() == str2.ToLower());//先转换成小写再进行比较
+            Console.WriteLine("str1.ToUpper() == str2.ToUpper()的结果为：{0}", str1.ToUpper() == str2.ToUpper());//先转换成大写再进行比较
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
