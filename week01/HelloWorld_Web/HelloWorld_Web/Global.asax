adcfc615dae38d971f3a35b43a373ae5c09819b3
yemuzhiqin <1957960965@qq.com>
2019-03-24 13:52:55 +0800

    新增加了第一周的作业

---
+++ week01/HelloWorld_Web/HelloWorld_Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="HelloWorld_Web.Global" Language="C#" %>
---
+++ week01/HelloWorld_WinForm/HelloWorld_WinForm/Form1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace HelloWorld_WinForm
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            MessageBox.Show(this, "Hello,World!这是一个C#Windows Form程序！", "提示信息", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+    }
+}
